621a38eb3e732fc7d0881c60de50560d86754d82
HazemEl-masry <hazemelmasryofficial@gmail.com>
2023-06-22 02:01:13 +0300

    custom outline

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/components/custom_outline.dart';
 import 'package:movie_app/constant/constant.dart';
 import 'dart:ui';
 
@@ -59,6 +60,39 @@ class OnBoardingScreen extends StatelessWidget {
                     ),
                   ),
                 )
+            ),
+            Column(
+              children: [
+                SizedBox(
+                  height: screenHight*0.07,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                  child: CustomOutline(
+                      strokeWidth: 4,
+                      radius: screenWidth*0.8,
+                      gradient: LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [
+                            Constant.kPinkColor,
+                            Constant.kPinkColor.withOpacity(0),
+                            Constant.kGreenColor.withOpacity(0.1),
+                            Constant.kGreenColor,
+                          ],
+                        stops: const [0.2,0.4,0.6,1]
+                      ),
+                      width: 400,
+                      height: 350.0,
+                      padding: const EdgeInsets.all(4.0),
+                      child: Image(image: AssetImage(
+                          "assets/img-onboarding.png"),
+                        height: 350.0,
+                        width: 400.0,
+                      )
+                  ),
+                )
+              ],
             )
           ],
         ),
