3610962d52671de53e2c5021ce0aaa2dd2af0941
HazemEl-masry <hazemelmasryofficial@gmail.com>
2023-06-22 14:31:38 +0300

    ui almost finish

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -61,14 +61,15 @@ class OnBoardingScreen extends StatelessWidget {
                   ),
                 )
             ),
-            Column(
-              children: [
-                SizedBox(
-                  height: screenHight*0.07,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                  child: CustomOutline(
+            Center(
+              child: Column(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: screenHight*0.12,
+                  ),
+                  CustomOutline(
                       strokeWidth: 4,
                       radius: screenWidth*0.8,
                       gradient: LinearGradient(
@@ -82,17 +83,86 @@ class OnBoardingScreen extends StatelessWidget {
                           ],
                         stops: const [0.2,0.4,0.6,1]
                       ),
-                      width: 400,
-                      height: 350.0,
-                      padding: const EdgeInsets.all(4.0),
-                      child: Image(image: AssetImage(
-                          "assets/img-onboarding.png"),
-                        height: 350.0,
-                        width: 400.0,
-                      )
+                      width: screenWidth*0.8,
+                      height: screenHight*0.4,
+                      padding: const EdgeInsets.all(4),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(image: AssetImage("assets/img-onboarding.png"),
+                            fit: BoxFit.cover,
+                            alignment: Alignment.bottomLeft
+                          )
+                        ),
+                      ),
                   ),
-                )
-              ],
+                  SizedBox(
+                    height: screenHight*0.09,
+                  ),
+                  Text(
+                      "Watch movies in \n Virtual Reality",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: screenHight<=660? 18:34,
+                      fontWeight: FontWeight.w700,
+                      color: Constant.kWhiteColor.withOpacity(0.85),
+                    ),
+                  ),
+                  SizedBox(
+                    height: screenHight*0.05,
+                  ),
+                  Text(
+                    "Download and watch offline \n wherever you are",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: screenHight<=660? 12:16,
+                      fontWeight: FontWeight.w100,
+                      color: Constant.kWhiteColor.withOpacity(0.75),
+                    ),
+                  ),
+                  SizedBox(
+                    height: screenHight*0.04,
+                  ),
+                  CustomOutline(
+                      strokeWidth: 3,
+                      radius: 20,
+                      gradient: const LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [
+                            Constant.kPinkColor,
+                            Constant.kGreenColor,
+                          ],
+                      ),
+                      width: 160.0,
+                      height: 38.0,
+                      padding: const EdgeInsets.all(3),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              colors: [
+                                Constant.kPinkColor.withOpacity(0.3),
+                                Constant.kGreenColor.withOpacity(0.3),
+                              ]
+                          )
+                        ),
+                        child: Center(
+                          child: Text(
+                              "Sign up",
+                            style: TextStyle(
+                              fontSize: 15.0,
+                              color: Constant.kWhiteColor,
+                              letterSpacing: 0.37
+                            ),
+                          ),
+                        ),
+                      ),
+                  )
+                ],
+              ),
             )
           ],
         ),
